40ea943ff0695116fafc06947441c5f5a83bfcee
TukadaHiroki <tuki.14hj@gmail.com>
2023-01-03 08:13:40 +0900

    1/3　セクション３の１１まで学習

---
+++ section3/11.py
@@ -0,0 +1,26 @@
+s = 'test'
+print(s)
+print('hello')
+print("hello")
+print("I don't know")
+print('say "I don\'t know"')
+
+print('hello.\nHow are you?')
+print(r'C:\name\name')
+
+print("##############")
+print("""\
+line1
+line2
+line3\
+        """)
+print("##############")
+
+print('Hi.' * 3 + 'Mike.')
+print('Py''thon')
+prefix = 'py'
+print(prefix + 'thon')
+
+d = ('AAAAAAAAAAAAA'
+     'BBBBBBBBBBBBB')
+print(d)
