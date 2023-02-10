e859a7038696f48cba2af257b74172e77437a6ec
TukadaHiroki <tuki.14hj@gmail.com>
2023-02-10 21:54:28 +0900

    2/10セクション5の42まで学習

---
+++ section5/40.py
@@ -0,0 +1,7 @@
+#while else文
+count = 0
+while count < 5:
+    print(count)
+    count += 1
+else:
+    print('done')
---
+++ section5/41.py
@@ -0,0 +1,8 @@
+#input関数
+while True:
+    word = input('Enter:')
+    num = int(word)
+    if word == 100:
+        print('OK!!')
+        break
+    print('next')
