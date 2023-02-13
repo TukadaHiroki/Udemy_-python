ea3e408545cb598e2ad4247b1be348ef28baa530
TukadaHiroki <tuki.14hj@gmail.com>
2023-02-13 23:19:34 +0900

    2/13セクション5の51まで学習

---
+++ section5/49.py
@@ -0,0 +1,8 @@
+#関数の引数と返り値の宣言
+#nem: int = 10
+
+def add_num(a: int, b: int) -> int:
+    return a + b
+
+r = add_num(10, 20)
+print(r)
---
+++ section5/50.py
@@ -0,0 +1,8 @@
+#位置引数とキーワード引数とデフォルト引数
+def menu(entree='beef', drink='wine', dessert='ice'):
+    print('entree = ', entree)
+    print('drink = ', drink)
+    print('dessert = ', dessert)
+
+menu()
+#menu(entree='beef', drink='beer', dessert='ice')
